8ebed88bca615fbecbba6f89d9d5d83a6437c4c7
Jeferson Araujo <jaraujo@eaibrasil.com>
2020-11-25 21:05:28 -0300

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "reactstrap";
+import Image from "next/image";
+
 function Home() {
-  return <div>develop</div>;
+  return (
+    <div>
+      <Container>
+        <Row className="text-center">
+          <Col>
+            <h2 className="mt-5">Ambiente de desenvolvimento</h2>
+            <Image
+              src="/images/construct-develop.png"
+              alt="Ambiente de desenvolvimento"
+              width={500}
+              height={500}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
